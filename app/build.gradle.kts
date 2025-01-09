c3948fe1f7a3f287b62af000499c58f9083d93a5
saatvik0409 <mangalsaatvik@gmail.com>
2025-01-09 23:18:16 +0100

    created fragments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -38,6 +39,8 @@ android {
     }
     buildFeatures {
         compose = true
+        dataBinding = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -51,14 +54,19 @@ android {
 
 dependencies {
 
+
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
     implementation("androidx.activity:activity-compose:1.9.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
--- app/src/main/java/com/example/kaamchori/MainActivity.kt
+++ app/src/main/java/com/example/kaamchori/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.kaamchori
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,28 +13,60 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import com.example.kaamchori.databinding.LayoutMainActivityBinding
 import com.example.kaamchori.ui.theme.KaamchoriTheme
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: LayoutMainActivityBinding
+    private lateinit var navController: NavController
+
+
+    companion object {
+        private var TAG = "MAIN_ACTIVITY"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding = LayoutMainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        // Find the NavHostFragment and get its NavController
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.main_activity_nav_host) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Get reference to the BottomNavigationView from the layout
+        val navView: BottomNavigationView = binding.bottomNavView
+
+        // Set item icon tint list to null (optional, to disable default icon tinting)
+//        navView.itemIconTintList = null
+
+        // Set up the BottomNavigationView with the NavController for navigation
+        navView.setupWithNavController(navController)
     }
+
+
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KaamchoriTheme {
-        Greeting("Android")
-    }
-}
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    KaamchoriTheme {
+//        Greeting("Android")
+//    }
+//}
